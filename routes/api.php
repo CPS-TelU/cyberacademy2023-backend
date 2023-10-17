369e7a449211a73fecc17c2252b5d1501062070d
adamwisnup <wisnuadam21@gmail.com>
2023-10-17 19:28:24 +0700

    feat: add update, & countdown controller

--- routes/api.php
+++ routes/api.php
@@ -20,14 +20,15 @@ use App\Http\Controllers\AssistantController;
 //     return $request->user();
 // });
 
-Route::post('/academy', [AcademyController::class, 'store']) ->name('academy.store');
-Route::post('/assistant/register', [AssistantController::class, 'register']) ->name('assistant.register');
-Route::post('/assistant/login', [AssistantController::class, 'login']) ->name('assistant.login');
+Route::post('/academy', [AcademyController::class, 'store'])->name('academy.store');
+Route::post('/assistant/register', [AssistantController::class, 'register'])->name('assistant.register');
+Route::post('/assistant/login', [AssistantController::class, 'login'])->name('assistant.login');
+Route::get('/academy/quota', [AcademyController::class, 'countdownQuota'])->name('academy.quota');
 
 Route::middleware(['auth:sanctum'])->group(function () {
-    Route::get('/academy', [AcademyController::class, 'index']) ->name('academy.index');
-    Route::get('/academy/{id}', [AcademyController::class,'show']) ->name('academy.show');
-    Route::post('/assistant/logout', [AssistantController::class,'logout']) ->name('assistant.logout');
-    Route::delete('/academy/{id}', [AcademyController::class,'destroy']) ->name('academy.destroy');
-    
+    Route::get('/academy', [AcademyController::class, 'index'])->name('academy.index');
+    Route::get('/academy/{id}', [AcademyController::class, 'show'])->name('academy.show');
+    Route::patch('/academy/{id}', [AcademyController::class, 'update'])->name('academy.update');
+    Route::post('/assistant/logout', [AssistantController::class, 'logout'])->name('assistant.logout');
+    Route::delete('/academy/{id}', [AcademyController::class, 'destroy'])->name('academy.destroy');
 });
